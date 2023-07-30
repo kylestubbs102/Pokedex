8f13f7c59380174d7ac0f60d2d209acb1b4465cd
Kyle Stubbs <kylestubbs102@gmail.com>
2023-07-29 22:48:21 -0700

    Fixed app from crashing when unknown json field is present

--- app/src/main/java/com/example/pokedex/di/NetworkModule.kt
+++ app/src/main/java/com/example/pokedex/di/NetworkModule.kt
@@ -2,6 +2,8 @@ package com.example.pokedex.di
 
 import com.example.pokedex.data.remote.PokedexApi
 import com.example.pokedex.data.remote.PokedexApi.Companion.BASE_URL
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
@@ -11,7 +13,8 @@ import retrofit2.converter.jackson.JacksonConverterFactory
 val networkModule = module {
     single { provideLoggingInterceptor() }
     single { provideOkHttpClient(get()) }
-    single { provideRetrofit(get()) }
+    single { provideObjectMapper() }
+    single { provideRetrofit(get(), get()) }
     single { provideSectorApi(get()) }
 }
 
@@ -25,11 +28,17 @@ private fun provideOkHttpClient(
         .addInterceptor(httpLoggingInterceptor)
         .build()
 
+private fun provideObjectMapper(): ObjectMapper =
+    ObjectMapper().apply {
+        configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    }
+
 private fun provideRetrofit(
-    okHttpClient: OkHttpClient
+    okHttpClient: OkHttpClient,
+    objectMapper: ObjectMapper,
 ): Retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
-    .addConverterFactory(JacksonConverterFactory.create())
+    .addConverterFactory(JacksonConverterFactory.create(objectMapper))
     .client(okHttpClient)
     .build()
 
